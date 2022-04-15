b586ccafd73ee95cab639a17c487793aa600c1d6
AlexseyKab <help_me101@mail.ru>
2022-04-15 09:19:55 +0300

    commit

---
+++ src/EditableSpan.stories.tsx
@@ -0,0 +1,14 @@
+import {EditableSpan} from "./EditableSpan";
+import {action} from "@storybook/addon-actions";
+import React from "react";
+
+export default {
+    title: 'EditableSpan Component',
+    component: EditableSpan
+}
+
+const callback = action('Task Changed')
+
+export const EditableSpanBaseExample = (props: any) => {
+    return <EditableSpan value={'Start changed'} onChange={callback}/>
+}
